4b5a57bb12e6e856772a628671ae275eabe411c5
settofili <64550617+settofili@users.noreply.github.com>
2021-09-08 08:57:01 +0100

    Updates

---
+++ precache-manifest.dfcbcca27bfdd371e6056fef1fae5daa.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c82633ceff7a044ac8b15fb6178b5b2",
+    "url": "/pxb/index.html"
+  },
+  {
+    "revision": "5f7dfca061e37b22008d",
+    "url": "/pxb/static/css/2.0c17ed4c.chunk.css"
+  },
+  {
+    "revision": "feebaa1354a476888952",
+    "url": "/pxb/static/css/main.826c3704.chunk.css"
+  },
+  {
+    "revision": "5f7dfca061e37b22008d",
+    "url": "/pxb/static/js/2.2fccaaa9.chunk.js"
+  },
+  {
+    "revision": "eee71f1118581112383b412eccddf91d",
+    "url": "/pxb/static/js/2.2fccaaa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feebaa1354a476888952",
+    "url": "/pxb/static/js/main.f02a44e2.chunk.js"
+  },
+  {
+    "revision": "5494c29a340b72ee52db",
+    "url": "/pxb/static/js/runtime-main.3a7566f2.js"
+  },
+  {
+    "revision": "9d7610c6841d1541dcd4b8b6d98c45ad",
+    "url": "/pxb/static/media/commercial.9d7610c6.jpg"
+  },
+  {
+    "revision": "5292dbae055444834a0b40bcb26d61e8",
+    "url": "/pxb/static/media/commercial2.5292dbae.jpg"
+  },
+  {
+    "revision": "ff238240fcb1df13e2727a02297d3647",
+    "url": "/pxb/static/media/commercial3.ff238240.jpg"
+  },
+  {
+    "revision": "5862e1ffd8cef09c662ea359670e018e",
+    "url": "/pxb/static/media/commercial4.5862e1ff.jpg"
+  },
+  {
+    "revision": "30507cc8bd9592c402979dd52d90f43d",
+    "url": "/pxb/static/media/dbank.30507cc8.png"
+  },
+  {
+    "revision": "71e5ca63471b876d28313c7407d8e525",
+    "url": "/pxb/static/media/logo.71e5ca63.jpg"
+  },
+  {
+    "revision": "2cc81283756db63283c8705f4877f312",
+    "url": "/pxb/static/media/rent.2cc81283.jpg"
+  },
+  {
+    "revision": "7967e712cb94cce1b8e1a84d7e998619",
+    "url": "/pxb/static/media/rent1.7967e712.jpg"
+  },
+  {
+    "revision": "b72a4f6d6ac45bdd0f842971baecdb51",
+    "url": "/pxb/static/media/rent2.b72a4f6d.jpg"
+  },
+  {
+    "revision": "be2a12486d938ea12e7db92ecd22e36d",
+    "url": "/pxb/static/media/rent3.be2a1248.jpg"
+  },
+  {
+    "revision": "14ce0cfcc05952c961cd7604cf4f0bf7",
+    "url": "/pxb/static/media/rent4.14ce0cfc.jpg"
+  }
+]);
